9420fe965e6a7d3058b8de132564e062ff49c14a
codewithshamim <shamimislamonline@gmail.com>
2022-02-22 17:13:47 +0600

    api added

--- Module 33 (API, JSON,  Data load, dynamic website)/api.js
+++ Module 33 (API, JSON,  Data load, dynamic website)/api.js
@@ -1,1 +1,19 @@
-// JavaScript Object Notation---------------
+// JavaScript Object Notation--------------JSON
+function myJsonData() {
+    fetch('https://jsonplaceholder.typicode.com/users')
+        .then(response => response.json())
+        .then(data => getData(data))
+}
+
+// call 
+myJsonData()
+
+// ------------------------
+function getData(data) {
+    const ul = document.getElementById('list-item');
+    for (const user of data) {
+        const li = document.createElement('li');
+        li.innerHTML = user.email;
+        ul.appendChild(li);
+    }
+}
