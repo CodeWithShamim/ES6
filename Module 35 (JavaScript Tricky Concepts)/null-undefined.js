8e50f738362e4ed647c8b2462eec045c0e0880e9
codewithshamim <shamimislamonline@gmail.com>
2022-02-25 22:11:55 +0600

    null & undefined added

---
+++ Module 35 (JavaScript Tricky Concepts)/null-undefined.js
@@ -0,0 +1,14 @@
+// undefined----------------
+// 1. Variable value not assigned
+// 2. function but didn't write return
+// 3. function but didn't pass return value
+// 4. parameter that isn't passed
+// 5. property that doesn't exist in an object
+// 6.accessing array element out of range
+// 7. accessing deleted array element
+// 8. explicity set value to undefined
+
+
+// null-----------------------
+// 1. null only use for object value
+// EX= const Value = {name: 'shamim', id: null, class: '1st'};
