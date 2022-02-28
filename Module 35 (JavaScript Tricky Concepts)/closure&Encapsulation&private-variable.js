b51448a2dfd4237f31e99b699889469841f891e1
codewithshamim <shamimislamonline@gmail.com>
2022-02-28 19:14:19 +0600

    closure update

--- Module 35 (JavaScript Tricky Concepts)/closure&Encapsulation&private-variable.js
+++ Module 35 (JavaScript Tricky Concepts)/closure&Encapsulation&private-variable.js
@@ -1,19 +1,21 @@
 // closure&Encapsulation&private-variable
 function stopWatch() {
     let count = 0;
-    return function() {
-        count++;
-        return count;
+    return function(name) {
+        return `hello ${name}`
     }
 }
 // result1--------
-const result1 = stopWatch();
-console.log(result1());
-console.log(result1());
-console.log(result1());
+// const result1 = stopWatch();
+// console.log(result1());
+// console.log(result1());
+// console.log(result1());
 
-// result2-------------
-console.log('---------------------------------');
-const result2 = stopWatch();
-console.log(result2());
-console.log(result2());
+// // result2-------------
+// console.log('---------------------------------');
+// const result2 = stopWatch();
+// console.log(result2());
+// console.log(result2());
+
+const result3 = stopWatch();
+console.log(result3('shamim'));
