14e0e0b499728d3b998b6b0ff628e736c1d6827f
codewithshamim <shamimislamonline@gmail.com>
2022-02-28 19:49:11 +0600

    callback function added

--- Module 35 (JavaScript Tricky Concepts)/callback-function.js
+++ Module 35 (JavaScript Tricky Concepts)/callback-function.js
@@ -1,2 +1,21 @@
-const getGirlFriend = (name = "chokina") => "name";
-console.log(getGirlFriend());
+function wecomeMessage(name, greedHandler) {
+    // console.log(greedHandler);
+    greedHandler(name);
+}
+
+function greetMorning(name) {
+    console.log('Good morning', name);
+}
+
+function greetAfternoon(name) {
+    console.log('Good Afternoon', name);
+}
+
+function greetEvening(name) {
+    console.log('Good Evening', name);
+}
+
+// call function------------
+wecomeMessage('shamim', greetMorning);
+wecomeMessage('sakib', greetAfternoon);
+wecomeMessage('anamul', greetEvening);
